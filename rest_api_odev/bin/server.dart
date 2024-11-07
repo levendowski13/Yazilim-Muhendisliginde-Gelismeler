6d6e86e405155386720dbec0f77f8df13bd9297c
Levent ERGÖREN <112565143+levendowski13@users.noreply.github.com>
2024-11-07 15:18:53 +0300

    GET ile iki sayıyı toplayan ve döndüren endpoint eklendi
    REST API uygulamasına GET isteği yapan yeni bir endpoint eklendi. Bu endpoint, verilen '2' sayıyı toplayarak sonucunu hesaplayacak ve sonucu isteği atan yere döndürecek şekilde işlevsellik sağlandı.

--- rest_api_odev/bin/server.dart
+++ rest_api_odev/bin/server.dart
@@ -7,6 +7,26 @@ import 'package:shelf_router/shelf_router.dart';
 void main() async {
   final router = Router();
 
+
+  router.get('/api/add-two', (Request request) {
+    final num1 = num.tryParse(request.url.queryParameters['num1'] ?? '');
+    final num2 = num.tryParse(request.url.queryParameters['num2'] ?? '');
+
+    if (num1 == null || num2 == null) {
+      return Response.badRequest(
+        body: json.encode({'error': 'Geçerli sayılar girin.'}),
+        headers: {'Content-Type': 'application/json'},
+      );
+    }
+
+    final result = num1 + num2;
+
+    return Response.ok(
+      json.encode({'result': result}),
+      headers: {'Content-Type': 'application/json'},
+    );
+  });
+
   var server = await io.serve(router, InternetAddress.anyIPv4, 13);
   print('Server running on http://localhost:${server.port}');
 }
